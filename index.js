25842196c6b8f8a5e602de8a4fa0ce0969a5bce8
Benhin <benhinmwendwa7@gmail.com>
2024-03-30 23:02:32 +0300

    bowling

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 
-function game(){
+
 const record = [
     { year: "2015", result: "W"},
     { year: "2014", result: "N/A"},
     { year: "2013", result: "L"},
 
-  ];}
+  ];
 
 function superbowlWin(record) {
-    const win = record.find(game => game.result === "W");
+    const win = record.find( bowl => bowl.result === "W");
 
     if (win) {
       return win.year;
